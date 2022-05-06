ff3d84727bf275d75193bced3f640697fd175bfe
thesaddest <youngscarlxrd@yandex.by>
2022-05-06 16:55:22 +0200

    adding checkRole middleware

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function(role) {
+    return function (req, res, next) {
+        if(req.method === "OPTIONS") {
+            next()
+        }
+        try {
+            const token = req.headers.authorization.split(' ')[1] //Bearer neededToken
+            if(!token) {
+                return res.status(401).json({message: "User is not authorised"})
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY)
+            if (decoded.role !== role) {
+                res.status(403).json({message: "Access denied"})
+
+            }
+            req.user = decoded
+            next()
+        } catch (e) {
+            res.status(401).json({message: "User is not authorised"})
+        }
+    }
+}
+
+
+
