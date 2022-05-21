b787901b9d6696a019be9cae8267bc7d181cb4e2
thesaddest <youngscarlxrd@yandex.by>
2022-05-21 18:26:36 +0200

    adding a cart mockup

---
+++ client/e-commerce-web-shop/src/pages/Basket/Basket.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import cl from './Basket.module.css'
+import {Link} from "react-router-dom";
+import {SHOP_ROUTE} from "../../utils/consts";
+import {Button} from "react-bootstrap";
+
+
+const Basket = () => {
+    const item = JSON.parse(localStorage.getItem('cart'))
+
+    return (
+        <div className={cl.mainContainer}>
+            <h2>Shopping Cart</h2>
+            {item === null ? (
+                <div className={cl.cartEmpty}>
+                    <p>Your cart is empty</p>
+                    <div className={cl.startShopping}>
+                        <Link to={SHOP_ROUTE}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+
+                                className="bi bi-arrow-left"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                                />
+                            </svg>
+                            <span>Start Shopping</span>
+                        </Link>
+                    </div>
+                </div>
+            ) : (<div>
+                    <div className={cl.cartTitles}>
+                        <h3 className={cl.itemTitle}>Item</h3>
+                        <h3 className={cl.itemPrice}>Price</h3>
+                        <h3 className={cl.itemQuantity}>Quantity</h3>
+                        <h3 className={cl.itemTotal}>Total</h3>
+                    </div>
+                    <div className={cl.cartItems}>
+                        {item?.map(cartItem =>
+                            <div className={cl.cartItem} key={cartItem.id}>
+                                <div className={cl.cartProduct}>
+                                    <img src={process.env.REACT_APP_API_URL + cartItem.img1} alt={cartItem.name}/>
+                                    <div>
+                                        <h3>{cartItem.name}</h3>
+                                        <p>{cartItem.menuTextForTabs1}</p>
+                                        <button>Remove</button>
+                                    </div>
+                                </div>
+                                <div className={cl.cartItemPrice}>${cartItem.price}</div>
+                                <div className={cl.cartItemQuantity}>
+                                    <button>-</button>
+                                    <div className={cl.itemsInCartCount}>{item.length}</div>
+                                    <button>+</button>
+                                </div>
+                                <div className={cl.cartItemTotalPrice}>
+                                    ${cartItem.price + item.length}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className={cl.cartSummary}>
+                        <button className={cl.clearCart}>
+                            Clear Cart
+                        </button>
+                        <div className={cl.cartCheckout}>
+                            <div className={cl.subtotal}>
+                                <span>Subtotal</span>
+                                <span className={cl.subtotalAmount}></span>
+                            </div>
+                            <p>Taxes and shipping are calculated at checkout</p>
+                            <button>Check out</button>
+                            <div className={cl.continueShopping}>
+                                <Link to={SHOP_ROUTE}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+
+                                        className="bi bi-arrow-left"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                                        />
+                                    </svg>
+                                    <span>Continue Shopping</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+
+};
+
+export default Basket;
--- client/e-commerce-web-shop/src/routes.js
+++ client/e-commerce-web-shop/src/routes.js
@@ -1,6 +1,6 @@
 import {ADMIN_ROUTE, BASKET_ROUTE, ITEM_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "./utils/consts";
 import Admin from "./pages/Admin/Admin";
-import Basket from "./pages/Basket";
+import Basket from "./pages/Basket/Basket";
 import Shop from "./pages/Shop";
 import Auth from "./pages/Auth/Auth";
 import ItemPage from "./pages/ItemPage/ItemPage";
