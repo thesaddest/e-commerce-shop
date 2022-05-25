6c073839cd2b0664514741a4187862c48b8bf481
thesaddest <youngscarlxrd@yandex.by>
2022-05-25 22:14:58 +0200

    changing client url

--- db.js
+++ db.js
@@ -10,8 +10,8 @@ module.exports = new Sequelize(
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
-                require: true, // This will help you. But you will see nwe error
-                rejectUnauthorized: false // This line will fix new error
+                require: true,
+                rejectUnauthorized: false
             }
         },
     }
