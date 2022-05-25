bd71bb55336052e475fe80de2a9aab585e615e5d
thesaddest <youngscarlxrd@yandex.by>
2022-05-25 20:42:09 +0200

    deploy x2

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const express = require('express')
+const models = require('./models/models')
+const sequelize = require('./db')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+//Errors handling always in the end, last Middleware
+app.use(errorHandler)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'e-commerce-web-shop', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'e-commerce-web-shop', 'build', 'index.html'))
+    })
+}
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`), console.log(process.env.NODE_ENV))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
